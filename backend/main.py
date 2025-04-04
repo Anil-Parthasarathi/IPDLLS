300a8c8d3d014d22fa0f19104921d5aa130468fc
Andres Santiago <AndyLSantiago20@gmail.com>
2025-04-04 16:40:11 -0500

    Added sending numpy 2d array as png

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import Response
+
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+import io
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    # Disparity would contain the output of the stereo matching algorithm
+    # Must be grayscale with dtype of uint8
+    disparity = (plt.imread("image.png") * 255).astype(np.uint8)[:, :, 0]
+
+    imgByteArr = io.BytesIO()
+    Image.fromarray(disparity, "L").save(imgByteArr, format="PNG")
+    imgByteArr = imgByteArr.getvalue()
+
+    return Response(content=imgByteArr, media_type="image/png")
